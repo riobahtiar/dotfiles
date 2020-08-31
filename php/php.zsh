e8d847f31f65257113df1ac4ac35c41b387e32de
Rio Bahtiar <akhibahtiar@gmail.com>
2020-08-31 15:54:46 +0700

    add wpcs and alias

---
+++ php/php.zsh
@@ -0,0 +1,4 @@
+alias phpcbf='/Users/rio/.composer/vendor/bin/phpcbf $*'
+alias phpcs='/Users/rio/.composer/vendor/bin/phpcs $*'
+alias wpcbf='/Users/rio/.composer/vendor/bin/phpcbf --standard="WordPress" $*'
+alias wpcs='/Users/rio/.composer/vendor/bin/phpcs --standard="WordPress" $*'
