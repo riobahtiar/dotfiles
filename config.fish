8852de25c019d40961beda9e58b5b9d6dd174e42
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2023-05-29 02:43:01 +0700

    refactor path method

--- config.fish
+++ config.fish
@@ -1,8 +1,7 @@
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
-
-export STARSHIP_CONFIG="$HOME/.config/fish/starship.toml"
-# source ~/.iterm2_shell_integration.fish
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH $VOLTA_HOME/bin $PATH
+set -gx PATH $PATH /Applications/Docker.app/Contents/Resources/bin/
+set -gx STARSHIP_CONFIG "$HOME/.config/fish/starship.toml"
 
 for file in ~/.config/fish/functions/custom/*.fish
     source $file
