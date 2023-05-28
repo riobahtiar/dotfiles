632d130089657fa4a664c695634031b2cd450a2f
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2023-05-29 02:03:38 +0700

    add git shortcut to push data

--- functions/custom/git.fish
+++ functions/custom/git.fish
@@ -19,3 +19,12 @@ end
 function g-new
     git checkout -b $argv
 end
+
+# git push origin to current branch
+function g-push
+    set -l branch_name (git symbolic-ref --short HEAD)
+    set -l answer (read -P "Are you sure you want to push changes to branch $branch_name? [y/N] ")
+    if test $answer = "y"
+        git push origin $branch_name
+    end
+end
